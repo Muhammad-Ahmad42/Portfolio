ed1d927a92866c093ef9edc889e90cf91bf140f5
Muhammad Ahmad <meahmadkhanalone@gmail.com>
2025-10-17 14:31:50 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-
-  images: {
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 60,
-  },
-
-  experimental: {
-    optimizeCss: true,
-  },
-
-  poweredByHeader: false,
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
